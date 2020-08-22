2d4c86e0253a319af1cc2aabae0afab1f52ec261
@ndre <codingdevhub@gmail.com>
2020-08-22 13:03:09 +0300

    added base template

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return 'Hello World!'
+    return render_template("index.html")
 
 
 @app.route('/about')
 def about():
-    return 'About company!'
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
