94ccd31eac5176bafeccee5af996b7cf6e9a27e7
@ndre <codingdevhub@gmail.com>
2020-08-23 22:58:34 +0300

    added new template, added new bootstrap style

--- app.py
+++ app.py
@@ -14,5 +14,25 @@ def about():
     return render_template("about.html")
 
 
+@app.route('/catalog')
+def catalog():
+    return render_template("catalog.html")
+
+
+@app.route('/catalog_pvc')
+def catalog_pvc():
+    return render_template("catalog_pvc.html")
+
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template("contacts.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
